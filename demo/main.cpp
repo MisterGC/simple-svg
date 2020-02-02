e16de0fe11093731ad3c06ec488884207d88257a
Serein Pfeiffer <serein.pfeiffer@gmail.com>
2020-02-02 18:51:48 +0100

    Structure: Separate demo and lib, provide header-only CMake cfg.

--- demo/main.cpp
+++ demo/main.cpp
@@ -30,7 +30,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 ******************************************************************************/
 
-#include "simple_svg.h"
+#include <simple-svg-writer/simple_svg.h>
 
 using namespace svg;
 
